b3e6fb2b46e46d8a6c1d846d98d6a1a4b01c97dc
Romel Subia <subiaromel1403@gmail.com>
2026-04-30 01:43:29 -0500

    Fix CAPA 7 memory engine directory creation

--- 00_SYSTEM/brain/runtime/memory_engine.ps1
+++ 00_SYSTEM/brain/runtime/memory_engine.ps1
@@ -5,14 +5,22 @@
         [string]$Value
     )
 
-    $MemFile = Join-Path $RootPath "00_SYSTEM\brain\memory\MEMORY_STORE.json"
+    $MemDir = Join-Path $RootPath "00_SYSTEM\brain\memory"
+    $MemFile = Join-Path $MemDir "MEMORY_STORE.json"
+
+    if (!(Test-Path $MemDir)) {
+        New-Item -ItemType Directory -Force -Path $MemDir | Out-Null
+    }
 
     if (!(Test-Path $MemFile)) {
-        $Init = @{}
-        $Init | ConvertTo-Json -Depth 10 | Set-Content $MemFile -Encoding UTF8
+        @{} | ConvertTo-Json -Depth 10 | Set-Content $MemFile -Encoding UTF8
     }
 
     $Data = Get-Content $MemFile -Raw | ConvertFrom-Json
+
+    if ($null -eq $Data) {
+        $Data = @{}
+    }
 
     $Data[$Key] = $Value
 
